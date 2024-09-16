e2e2d7d808ed6c4544a73330bc15b28cdda4fe68
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-09-16 13:24:22 +0200

    creacion clase Telefon

---
+++ src/main/java/nivell2/Telefon.java
@@ -0,0 +1,12 @@
+package nivell2;
+
+public class Telefon {
+
+    private String marca;
+    private String model;
+
+    public void trucar(String numTelefon) {
+        System.out.println("S'està trucant al número " + numTelefon);
+    }
+
+}
